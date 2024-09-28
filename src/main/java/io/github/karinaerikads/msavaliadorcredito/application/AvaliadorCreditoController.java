fef7566d6d97d1fb593f37c03425ee74efca1c17
karinaerikads <karinaerikads48@gmail.com>
2024-09-27 22:09:58 -0300

    manda mensagem pra fila pra solicitar cartão

--- src/main/java/io/github/karinaerikads/msavaliadorcredito/application/AvaliadorCreditoController.java
+++ src/main/java/io/github/karinaerikads/msavaliadorcredito/application/AvaliadorCreditoController.java
@@ -2,10 +2,8 @@ package io.github.karinaerikads.msavaliadorcredito.application;
 
 import io.github.karinaerikads.msavaliadorcredito.application.exception.DadosClienteNotFoundException;
 import io.github.karinaerikads.msavaliadorcredito.application.exception.ErroComunicacaoMicroserviceException;
-import io.github.karinaerikads.msavaliadorcredito.domain.model.DadosAvaliacao;
-import io.github.karinaerikads.msavaliadorcredito.domain.model.DadosCliente;
-import io.github.karinaerikads.msavaliadorcredito.domain.model.RetornoAvaliacaoCliente;
-import io.github.karinaerikads.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.github.karinaerikads.msavaliadorcredito.application.exception.ErroSolicitacaoCartaoException;
+import io.github.karinaerikads.msavaliadorcredito.domain.model.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -25,7 +23,7 @@ public class AvaliadorCreditoController {
     }
 
     @GetMapping(value = "situacao-cliente", params = "cpf")
-    public ResponseEntity consultaSituacaoCliente(@RequestParam("cpf") String cpf){
+    public ResponseEntity consultarSituacaoCliente(@RequestParam("cpf") String cpf){
         SituacaoCliente situacaoCliente = null;
         try {
             situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
@@ -46,6 +44,16 @@ public class AvaliadorCreditoController {
             return ResponseEntity.notFound().build();
         } catch (ErroComunicacaoMicroserviceException e) {
             return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("solicitacoes-cartao")
+    public ResponseEntity solicitarCartao(@RequestBody DadosSolicitacaoEmissaoCartao dados){
+        try {
+            ProtocoloSolicitacaoCartao protocoloSolicitacaoCartao = avaliadorCreditoService.solicitarEmissaoCartao(dados);
+            return ResponseEntity.ok(protocoloSolicitacaoCartao);
+        }catch (ErroSolicitacaoCartaoException e){
+            return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
 }
--- src/main/java/io/github/karinaerikads/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ src/main/java/io/github/karinaerikads/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -3,9 +3,11 @@ package io.github.karinaerikads.msavaliadorcredito.application;
 import feign.FeignException;
 import io.github.karinaerikads.msavaliadorcredito.application.exception.DadosClienteNotFoundException;
 import io.github.karinaerikads.msavaliadorcredito.application.exception.ErroComunicacaoMicroserviceException;
+import io.github.karinaerikads.msavaliadorcredito.application.exception.ErroSolicitacaoCartaoException;
 import io.github.karinaerikads.msavaliadorcredito.domain.model.*;
 import io.github.karinaerikads.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import io.github.karinaerikads.msavaliadorcredito.infra.clients.ClienteResourceClient;
+import io.github.karinaerikads.msavaliadorcredito.infra.mqueue.SolicitacaoEmissaoCartaoPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,6 +24,7 @@ public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clienteResourceClient;
     private final CartoesResourceClient cartoesResourceClient;
+    private final SolicitacaoEmissaoCartaoPublisher emissaoCartaoPublisher;
     public SituacaoCliente obterSituacaoCliente(String cpf)
             throws DadosClienteNotFoundException, ErroComunicacaoMicroserviceException{
         try {
@@ -71,6 +75,16 @@ public class AvaliadorCreditoService {
                 throw new DadosClienteNotFoundException();
             }
             throw new ErroComunicacaoMicroserviceException(e.getMessage(), status);
+        }
+    }
+
+    public ProtocoloSolicitacaoCartao solicitarEmissaoCartao(DadosSolicitacaoEmissaoCartao dados){
+        try {
+            emissaoCartaoPublisher.solicitarCartao(dados);
+            var protocolo = UUID.randomUUID().toString();
+            return new ProtocoloSolicitacaoCartao(protocolo);
+        }catch (Exception e){
+            throw new ErroSolicitacaoCartaoException(e.getMessage());
         }
     }
 }
